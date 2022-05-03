ea82f96ada673351df3181befdea019e15edb499
shion0625 <xkaito0912@gmail.com>
2022-05-03 16:55:08 +0900

    validate完成

--- src/app/Http/Requests/Student/UpdateStudentRequest.php
+++ src/app/Http/Requests/Student/UpdateStudentRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
-class StoreStudentRequest extends FormRequest
+class UpdateStudentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -26,30 +26,28 @@ class StoreStudentRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => ['required','string','max:100'],
-            'age' => ['required','numeric','regex:/^[0-9][0-9]*/'],
-            'sex' => ['required','string','max:20'],
-            'email' => ['required','email:strict,dns,spoof','max:255'],
-            'course' => ['required','string','max:100']
+            'name' => ['string','max:100'],
+            'age' => ['numeric','regex:/^[0-9][0-9]*/'],
+            'sex' => ['string','max:20'],
+            'email' => ['email:strict,dns,spoof','max:255'],
+            'course' => ['string','max:100']
         ];
     }
 
         public function messages()
     {
         return [
-            'name.required' => '名前を入力してください。',
+
             'name.string' => '名前を文字列で入力してください。',
             'name.max' => '名前の最大文字数は100文字です。',
-            'age.required' => '年齢を入力してください。',
+
             'age.numeric' => '数字または数値形式の文字列で入力してください。',
             'age.regex' => '0以上の値を入力してください。',
-            'sex.required' => '性別を入力してください。',
+
             'sex.string' => '性別をを文字列で入力してください。',
             'sex.max' => '名前の最大文字数は20文字です。',
-            'email.required' => 'メールアドレスを入力してください。',
             'email.email' => 'メールアドレスを正しい形式で入力してください。',
             'email.max' => 'メールアドレスの最大文字数は255文字です。',
-            'course.required' => 'コース名を入力してください。',
             'course.string' => 'コース名を文字列で入力してください。',
             'course.max' => 'コース名の最大文字数は100文字です。',
         ];
@@ -59,7 +57,7 @@ class StoreStudentRequest extends FormRequest
     {
         $response = response()->json([
             'status' => 400,
-            'message' => "records create failure",
+            'message' => "records update failure",
             'errors' => $validator->errors(),
         ],400);
         throw new HttpResponseException($response);
